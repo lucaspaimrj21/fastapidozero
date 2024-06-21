19b049b1039c84ba32ed42a4b8e77a1d5c3a6507
Lucas Paim <lucaspaimrj21@gmail.com>
2024-06-20 22:30:39 -0300

    adicionando rotas do CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,64 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_o_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root_deve_retornar_o_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'lucas',
+            'email': 'lucas@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'lucas',
+        'email': 'lucas@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'lucas',
+                'email': 'lucas@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': 'secret',
+            'username': 'lucas2',
+            'email': 'lucas@example.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'lucas2',
+        'email': 'lucas@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
