b628e4a31da502e6cc01b0d2e3f560d4a8caf594
Lucas Paim <lucaspaimrj21@gmail.com>
2024-06-23 17:47:29 -0300

    exercícios da aula 3 realizados

--- tests/test_app.py
+++ tests/test_app.py
@@ -40,25 +40,64 @@ def test_read_users(client):
     }
 
 
+def test_read_one_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'lucas',
+        'email': 'lucas@example.com',
+        'id': 1,
+    }
+
+
+def test_read_one_user_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
         json={
             'password': 'secret',
-            'username': 'lucas2',
-            'email': 'lucas@example.com',
+            'username': 'pedro',
+            'email': 'pedro@example.com',
             'id': 1,
         },
     )
 
     assert response.json() == {
-        'username': 'lucas2',
-        'email': 'lucas@example.com',
+        'username': 'pedro',
+        'email': 'pedro@example.com',
         'id': 1,
     }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'password': 'secret',
+            'username': 'pedro',
+            'email': 'pedro@example.com',
+            'id': 777,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_delete_user(client):
     response = client.delete('/users/1')
 
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.json() == {'detail': 'User not found'}
